a86da9d3361ad0610b6e7bdabb6f6818ad658607
NeimadTL <damientalbot26@gmail.com>
2026-01-10 17:29:43 -0400

    Study chapter 11
    Chapter 11 shows the use of globale variables and type definitions
    which latter allows one to create their own type types instead of
    using the ones provided by C (int, char...etc).

---
+++ ch11_More-about-types-and-variables/enumerated_types.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+typedef enum {
+  false,
+  true
+} BOOLEAN;
+
+int main (void)
+{
+  BOOLEAN b_var;
+  b_var = false;
+
+  if (b_var == true) printf("TRUE\n");
+  else printf("FALSE\n");
+}
+
---
+++ ch11_More-about-types-and-variables/global_variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int result;
+
+void add(int a, int b)
+{
+  result = a + b;
+}
+
+int main (void)
+{
+  add(3, 4),
+  printf("The result is %d\n", result);
+}
+
