781c3c2a148ce7281379c786fcfc7a76ad1bc87f
NeimadTL <damientalbot26@gmail.com>
2026-01-11 18:13:26 -0400

    Study chapter 12 (last part)
    Apart from defining constant, the `#define`directive can also be
    used to define functions.
    
    The `#if` directive also exists so that code can be executed or
    not. I wonder how this is different from standard if statement?
    Maybe a memory thing as for constant the define directive.
    
    It's important to note that when gcc is called, it does two things:
    first it compilees the C source file into what's called an object
    file and then it links the object file with all the library
    functions to create the executable. The second step is performed by
    a program called linker.

--- ch12_Header-files-and-the-preprocessor/define_directive_example.c
+++ ch12_Header-files-and-the-preprocessor/define_directive_example.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 
 #define PI 3.14159
+#define ADD(a,b) (a+b)
 
 int main (void)
 {
+  // use of #define constant
   float rad = 3;
   float circ = rad * 2 * PI;
   float area = rad * rad * PI;
-  printf("The circumference of a circle radisu %f is %f\n", rad, circ);
+  printf("The circumference of a circle radius %f is %f\n", rad, circ);
   printf("The area of a circle radius %f is %f\n", rad, area);
+
+
+  // use of #define function
+  printf("The sum of %d and %d is %d\n", 5, 2, ADD(5, 2)); 
+  printf("The sum of %d and %d is %d\n", 8, 10, ADD(8, 10));
+
+
+  // use of #if directive
+  #if 0
+    printf("THIS IS NOT EXECUTED");
+  #else
+    printf("THIS IS EXECUTED");
+  #endif
+
 }
 
